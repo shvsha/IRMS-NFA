93c42880b427ed7ec4fba9c5f47226f3ad07b1a8
Vi <vivladine@gmail.com>
2026-03-08 14:32:41 +0800

    initial report status structure

--- src/components/warehouse supervisor/ReportStatus.jsx
+++ src/components/warehouse supervisor/ReportStatus.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function ReportStatus() {
   return (
-    <div>Report Status</div>
-  )
+    <>
+      <div className="whole-container-status">
+        <div className="filter-container-status">
+          <div className="table-wrapper-status">
+            <table className="table-container-status">
+              <thead>
+                <tr>
+                  <th>Report ID</th>
+                  <th>Report Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* Table rows would be populated with report data */}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
